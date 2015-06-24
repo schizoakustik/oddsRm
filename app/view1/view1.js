f5d733e392c2f9b69010d65c6e36ad6ba200c323
schizoakustik <schizoakustik@schizoakustik.se>
2015-06-24 16:03:39 +0200

    initial commit

---
+++ app/view1/view1.js
@@ -0,0 +1,38 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+}])
+
+.controller('View1Ctrl',
+    ['$scope',
+      function($scope) {
+        $scope.players = [
+          {
+            name: "Linus",
+            image: "../img/linus.png",
+            winnings: -200
+          },
+          {
+            name: "Karl",
+            image: "../img/karl.png",
+            winnings: -200
+          },
+          {
+            name: "Petter",
+            image: "../img/petter.png",
+            winnings: -187
+          }
+        ];
+
+        $scope.editWinnings = function(index, value) {
+            $scope.players[index].winnings = value;
+        };
+
+    }
+]);
